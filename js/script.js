c10722c75547165149374dcc3b80c8e584a369d3
32Fire32 <nicola.gianfelici@hotmail.it>
2022-11-14 15:14:23 +0100

    aggiunge bottoni next prev

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
-'strict mode'
+const {createApp} = Vue
 
-const { createApp } = Vue
-                         
 createApp({
-    data() {
+    data(){
         return {
+            activeImage: 0,
             slides: [
-                {
-                    image: 'img/01.webp',
+                    {
+                        image: 'img/01.webp',
                         title: 'Marvel\'s Spiderman Miles Morale',
                         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
                     }, {
@@ -29,5 +28,20 @@ createApp({
                     }
                 ]
         }
+    },
+    methods: {
+        prevImage(){
+            this.activeImage--
+            if( this.activeImage < 0 ) {
+                this.activeImage = this.slides.length - 1
+            } 
+        },
+
+        nextImage(){
+            this.activeImage++
+            if( this.activeImage > this.slides.length -1){
+                this.activeImage = 0;
+            }
+        }
     }
-}).mount('#app')
+}).mount('#app');
